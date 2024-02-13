18f7fe986db4d16024b16d0fff6497746f455f80
Abdalla Akal <araji002@email.cpcc.edu>
2024-02-12 23:32:02 -0500

    Update helloworld.java
    Description

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!"); // Original print statement
+        System.out.println("Program modified, but able to revert to original version using GitHub Desktop."); // Additional print statement
     }
 }
